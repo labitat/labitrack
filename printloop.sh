0f9bc9b789055023fbc91d6087f7a1d6901ecd1d
Asbjørn Sloth Tønnesen <asbjorn@asbjorn.biz>
2017-11-15 20:19:23 +0000

    2016: improve printloop (eg. handle offline printer)
    Signed-off-by: Asbjørn Sloth Tønnesen <asbjorn@asbjorn.biz>

--- printloop.sh
+++ printloop.sh
@@ -4,12 +4,11 @@ printer="/dev/labelprinter"
 queuedir=queue
 
 print_queue(){
-	for job in $(ls -1 "$queuedir/new"); do
-		./trigger.sh "$printer" "$queuedir"
-	done
+	./trigger.sh "$printer" "$queuedir"
 }
 
 print_queue
-while inotifywait --quiet --timeout 30 "$queuedir/new"; do
+while true; do
+	inotifywait --quiet --timeout 30 "$queuedir/new"
 	print_queue
 done
--- trigger.sh
+++ trigger.sh
@@ -6,7 +6,18 @@ printed_dir=$queue_dir/printed/
 
 mkdir -p $printed_dir
 
-for file in $(ls $queue_dir/new/* 2>/dev/null); do
+printqueue(){
+	ls $queue_dir/new/* 2>/dev/null
+}
+
+queuedepth=$(printqueue | wc -l)
+
+if [ ! -c "$printer" ] ; then
+	printf "labelprinter is offline, there are %d jobs in queue\n" $queuedepth >&2
+	exit 1
+fi
+
+for file in $(printqueue); do
 	echo printing $file
 	./ql570/ql570 $printer $file
 	status=$?
